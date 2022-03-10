8899015eb6cf1835ef89599406591ffa1d7107bd
Mohammad Kashif <83182299+M0hammad-Kashif@users.noreply.github.com>
2022-03-10 19:32:08 +0530

    Time: 88 ms (89.33%), Space: 46.6 MB (76.11%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -13,7 +13,7 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size()-1;i++)
         {
-            if(nums[i] >= nums[i+1])
+            if(nums[i] == nums[i+1])
                 return true;
         }
         return false;
